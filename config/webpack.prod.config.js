dc03795b95b0a674d7e5f75fbb39df00e8215989
LiegardP <pierre.liegard@gmail.com>
2020-05-07 10:39:14 +0200

    add webpack monitor, bundle analyzer - work on nunjucks implementation

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,13 +1,25 @@
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WebpackMonitor = require('webpack-monitor');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpackBaseConfig = require('./webpack.common.config.js');
 
 module.exports = merge(webpackBaseConfig, {
- optimization: {
-   minimizer: [
-     new UglifyJsPlugin(),
-     new OptimizeCSSAssetsPlugin()
-   ]
- }
+	plugins: [
+		new BundleAnalyzerPlugin(),
+		new WebpackMonitor({
+            capture: true, // -> default 'true'
+            target: '../monitor/myStatsStore.json', // default -> '../monitor/stats.json'
+            launch: true, // -> default 'false'
+            port: 8081, // default -> 8081
+            excludeSourceMaps: true // default 'true'
+          })
+	],
+	optimization: {
+		minimizer: [
+		new UglifyJsPlugin(),
+		new OptimizeCSSAssetsPlugin()
+		]
+	}
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,13 @@
 import './../scss/main.scss';
-// import webpackgif from '../images/webpack.gif';
+import './../css/style.css';
 import { Header } from './header';
 import { Footer } from './../ts/footer';
 
 let header = new Header();
-let firstHeading = header.getFirstHeading();
+let firstHeading = header.getHeaderText();
 console.log(firstHeading);
 
 let footer = new Footer();
 let footerText = footer.getFooterText();
 console.log(footerText);
 
-// setting the source of img
-// document.getElementById('webpack-gif').setAttribute('src', webpackgif);
